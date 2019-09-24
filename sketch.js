10e5ea0365856dc66cd0a92931342e259dcb3416
yueling-juri <55393310+yueling-juri@users.noreply.github.com>
2019-09-24 22:47:44 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight)
+  background(3,28,252);
+  angleMode(DEGREES);
+
+  frameRate(12);
 }
 
 function draw() {
-  // put drawing code here
+
+  //line
+  push();
+  noFill();
+  stroke('#f9b693');
+  translate(width/2,height/2);
+
+  //rotate
+  stroke('#f9b693');
+  line(100,-100,cos(frameCount*3)*200,sin(frameCount*3)*200);
+  pop();
+
+  if (frameCount == 240) {
+    noLoop();
+  }
 }
